c531459e453589795503d441781462abd027fe2f
Hasnain <99191648+HasnainHSA@users.noreply.github.com>
2022-02-28 22:12:57 +0500

    Add files via upload

---
+++ javascript 1st class/app.js
@@ -0,0 +1,22 @@
+// ye string ha 
+var string= "Hasnain"
+console.log(string)
+
+// ye integer ha 
+var integer= 123
+console.log(integer)
+
+
+// ye boolean ha 
+var boolean=true
+console.log(boolean)
+
+
+// ye decimal ha 
+var decimal= 2378.5764
+console.log(decimal)
+
+
+// ye array ha 
+var array=[1,2,3,4,6,67,string]
+console.log(array)
